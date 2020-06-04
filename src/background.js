87efda21f1aeb536e7ed0e7dd3031abd91557560
eskye <sunkanmiijatuyi@gmail.com>
2020-06-04 06:40:31 +0100

    setup video stream

---
+++ src/background.js
@@ -0,0 +1,76 @@
+let desktopMediaRequestId = '';
+
+chrome.runtime.onConnect.addListener(port => {
+  port.onMessage.addListener(msg => {
+    if (msg.type === 'SS_UI_REQUEST') {
+      requestScreenSharing(port, msg);
+    }
+    if (msg.type === 'SS_UI_CANCEL') {
+      cancelScreenSharing(msg);
+    }
+  });
+});
+
+function requestScreenSharing(port, msg) {
+  // https://developer.chrome.com/extensions/desktopCapture
+  // params:
+  //  - 'data_sources' Set of sources that should be shown to the user.
+  //  - 'targetTab' Tab for which the stream is created.
+  //  - 'streamId' String that can be passed to getUserMedia() API
+  // Also available:
+  //  ['screen', 'window', 'tab', 'audio']
+  const sources = ['screen', 'window', 'tab', 'audio'];
+  const tab = port.sender.tab;
+
+  desktopMediaRequestId = chrome.desktopCapture.chooseDesktopMedia(
+    sources,
+    port.sender.tab,
+    streamId => {
+      if (streamId) {
+        msg.type = 'SS_DIALOG_SUCCESS';
+        msg.streamId = streamId;
+      } else {
+        msg.type = 'SS_DIALOG_CANCEL';
+      }
+      port.postMessage(msg);
+    }
+  );
+}
+
+function cancelScreenSharing(msg) {
+  if (desktopMediaRequestId) {
+    chrome.desktopCapture.cancelChooseDesktopMedia(desktopMediaRequestId);
+  }
+}
+
+function flatten(arr) {
+  return [].concat.apply([], arr);
+}
+
+// This avoids a reload after an installation
+chrome.windows.getAll({ populate: true }, windows => {
+  const details = { file: 'content.js', allFrames: true };
+
+  flatten(windows.map(w => w.tabs)).forEach(tab => {
+    // Skip chrome:// pages
+    if (tab.url.match(/(chrome):\/\//gi)) {
+      return;
+    }
+
+    // https://developer.chrome.com/extensions/tabs#method-executeScript
+    // Unfortunately I don't know how to skip non authorized pages, and
+    // executeScript doesn't have an error callback.
+    chrome.tabs.executeScript(tab.id, details, () => {
+      const { runtime: { lastError } } = chrome;
+
+      if (
+        lastError &&
+        !lastError.message.match(/cannot access contents of url/i)
+      ) {
+        console.log(lastError);
+      }
+
+      console.log('After injection in tab: ', tab);
+    });
+  });
+});
--- src/content.js
+++ src/content.js
@@ -1,29 +1,45 @@
-chrome.runtime.onMessage.addListener((request) =>{
-    const video = document.createElement("VIDEO");
-    let recorder, stream;
-    debugger;
-  console.log(video);
-    async function startRecording() {
-      stream = await navigator.mediaDevices.getDisplayMedia({
-        video: { mediaSource: "screen" },
-        audio: {
-          echoCancellation: true,
-          noiseSuppression: true,
-          sampleRate: 44100
-        }
-      });
-      console.log(stream);
-      recorder = new MediaRecorder(stream);
-    
-      const chunks = [];
-      recorder.ondataavailable = e => chunks.push(e.data);
-      recorder.onstop = e => {
-        const completeBlob = new Blob(chunks, { type: chunks[0].type });
-        video.src = URL.createObjectURL(completeBlob);
-      };
-    
-      recorder.start();
-      startRecording();
-    } 
-    
-});
+// https://chromeextensionsdocs.appspot.com/apps/content_scripts#host-page-communication
+//   - 'content_script' and execution env are isolated from each other
+//   - In order to communicate we use the DOM (window.postMessage)
+//
+// app.js            |        |content-script.js |      |background.js
+// window.postMessage|------->|port.postMessage  |----->| port.onMessage
+//                   | window |                  | port |
+// getUserMedia      |<------ |window.postMessage|<-----| port.postMessage
+//
+window.contentScriptHasRun = false;
+
+(function() {
+  // prevent the content script from running multiple times
+  if (window.contentScriptHasRun) {
+    return;
+  }
+
+  window.contentScriptHasRun = true;
+
+  const port = chrome.runtime.connect(chrome.runtime.id);
+  port.onMessage.addListener(msg => {
+    window.postMessage(msg, '*');
+  });
+
+  window.addEventListener(
+    'message',
+    event => {
+      // Only accept messages from ourselves
+      if (event.source !== window) {
+        return;
+      }
+      // Only accept events with a data type
+      if (!event.data.type) {
+        return;
+      }
+
+      if (['SS_UI_REQUEST', 'SS_UI_CANCEL'].includes(event.data.type)) {
+        port.postMessage(event.data);
+      }
+    },
+    false
+  );
+
+  window.postMessage({ type: 'SS_PING', text: 'start' }, '*');
+})();
